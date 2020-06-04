91d6d8cb871ba1480b9f5d591121c395eacec8c5
qme1519 <jakubek.mi@gmail.com>
2020-06-04 23:17:26 +0400

    Admin panel

--- user_interface/admin.py
+++ user_interface/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from user_interface.models import Type
+
+class TypeAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Type, TypeAdmin)
 
 # Register your models here.
